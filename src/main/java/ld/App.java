ca5ade1225a6fb4d2d05929dae2de70e302316d5
Arash Bannazadeh-Mahani <axbannaz@gmail.com>
2017-05-22 17:26:49 -0700

    Add more C apis

--- src/main/java/ld/App.java
+++ src/main/java/ld/App.java
@@ -8,7 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        Env.e.setenv("AVAR", "AVAL", 1);
+        try {
+                System.out.println( "Hello World!" );
+                Env.e.setenv("AVAR", "AVAL", 1);
+                System.out.println(Env.e.getpid());
+                System.out.println(Env.e.getppid());
+                long[] timenul = new long[1];
+                System.out.println(Env.e.time(timenul));
+            } catch (UnsatisfiedLinkError e) {
+                System.out.println("Exception" + e);
+            }
     }
 }
--- src/main/java/ld/Env.java
+++ src/main/java/ld/Env.java
@@ -7,6 +7,9 @@ public class Env {
   public interface Setenv extends Library {
     public int setenv(String name, String value, int overwrite);
     public int unsetenv(String name);
+    public int getpid();
+    public int getppid();
+    public long time(long buf[]);
   }
   static Setenv e = (Setenv) Native.loadLibrary("c", Setenv.class);
 }
