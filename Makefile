0313e8e45d1ad80877c736595a69931474e9ab0d
Arash Bannazadeh-Mahani <axbannaz@gmail.com>
2017-05-22 23:23:53 -0700

    adding addJavaLibraryPath() it helps to find the lib.. but not its dependents

--- Makefile
+++ Makefile
@@ -7,7 +7,8 @@ java:
 	mvn clean install assembly:single 
 
 test:
-	(cd target; java -Djava.library.path=$$(pwd)/linux-x86-64 -jar app-1.0-SNAPSHOT-jar-with-dependencies.jar )
+	(cd target; java -jar app-1.0-SNAPSHOT-jar-with-dependencies.jar )
+#	(cd target; java -Djava.library.path=$$(pwd)/linux-x86-64 -jar app-1.0-SNAPSHOT-jar-with-dependencies.jar )
 
 clean:
 	mvn clean
--- src/main/java/ld/App.java
+++ src/main/java/ld/App.java
@@ -1,6 +1,9 @@
 package ld;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.lang.reflect.Field;
+import java.lang.ClassLoader;
 
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.PointerByReference;
@@ -11,12 +14,43 @@ import com.sun.jna.ptr.PointerByReference;
  */
 public class App 
 {
+    public static void addJavaLibraryPath(String s) throws IOException {
+        try {
+            // This enables the java.library.path to be modified at runtime
+            // From a Sun engineer at http://forums.sun.com/thread.jspa?threadID=707176
+            //
+            Field fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
+            fieldSysPath.setAccessible( true );
+            fieldSysPath.set( null, null );
+//            Field field = ClassLoader.class.getDeclaredField("usr_paths");
+//            field.setAccessible(true);
+//            String[] paths = (String[])field.get(null);
+//            for (int i = 0; i < paths.length; i++) {
+//                if (s.equals(paths[i])) {
+//                    return;
+//                }
+//            }
+//            String[] tmp = new String[paths.length+1];
+//            System.arraycopy(paths,0,tmp,0,paths.length);
+//            tmp[paths.length] = s;
+//            field.set(null,tmp);
+            String os=new String(System.getProperty("java.library.path"));
+            System.setProperty("java.library.path", s+":"+os);
+        } catch (IllegalAccessException e) {
+            throw new IOException("Failed to get permissions to set library path");
+        } catch (NoSuchFieldException e) {
+            throw new IOException("Failed to get field handle to set library path");
+        }
+    }
+
     public static void main( String[] args )
     {
-       // try {
+       try {
                 System.out.println( "Hello World!" );
                 File p = new File("linux-x86-64");
                 MyApi.mysys.setenv("LD_LIBRARY_PATH", p.getAbsolutePath() ,1);
+                System.out.println("before java.library.path " + System.getProperty("java.library.path"));
+                addJavaLibraryPath(p.getAbsolutePath());
                 System.out.println(MyApi.mysys.getenv("LD_LIBRARY_PATH"));
                 System.out.println(System.getenv("AVAR"));
                 System.out.println(MyApi.mysys.getpid());
@@ -24,11 +58,16 @@ public class App
                 long[] timenul = new long[1];
                 System.out.println(MyApi.mysys.time(timenul));
 
+                System.out.println("after java.library.path " + System.getProperty("java.library.path"));
                 System.loadLibrary("myalloc");
                 System.loadLibrary("test");
                 System.out.println("yoYo");
                 File f = new File("linux-x86-64/libtest.so");
                 System.load(f.getAbsolutePath());
+       }
+       catch (IOException e){
+                System.out.println("Exception" + e);
+       }
 
 /*                int x;
                 PointerByReference pref = new PointerByReference();
